1f9b83755a7ecfd6e54b25144ed63a507953c1d0
Nathan Chamberlain <chamberlainuxdesign@gmail.com>
2023-02-01 14:24:27 -0800

    Add warm-up until Arrays

--- src/warm-up.js
+++ src/warm-up.js
@@ -3,6 +3,8 @@
 // become comfortable with these fundamental concepts. Do not move on to
 // iteration 1 until you are confident that you've completed all of iteration 0.
 
+const { should } = require("chai");
+
 //------------------------- Variables -------------------------//
 
 
@@ -17,23 +19,33 @@
 
 // Reassign (not initialize) your "inStock" variable from above. We got a new shipment! Use a built in math operator to make 
 // the inStock count double what it currently is.
-inStock = (10 * 2);
-console.log(inStock);
+ inStock = (10 * 2);
+
+    console.log(inStock);
 
 // Reassign (not initialize) your "inStock" variable from above. You got hungry and needed a snack! 
 // Use a built in math operator to decrement the inStock count by 1.
+ inStock--;
+
+    console.log(inStock);
 
 
 // Initialize a new variable "isFavorite", and assign it to a boolean value indicating
 // if the food you chose is your favorite food or not
 
-
+ isFavorite = false;
 
 // Create a new variable named "shouldOrderMore", and use a comparison operator to
 // assign a boolean value to this variable. If "inStock" is less than 20, the
 // variable should be true, otherwise it should be false. You MUST use a
 // comparison operator.
 
+    if(inStock < 20){
+        shouldOrderMore = true
+    } else {
+        shouldOrderMore = false
+    }
+        console.log(shouldOrderMore);
 
 
 //------------------------- Conditionals -------------------------//
@@ -44,14 +56,24 @@ console.log(inStock);
 // the statement 'I'm going to eat some [food]!', otherwise, log the statement 
 // 'We need to get some more [food].'
 
+ var stockCount = 10;
 
+    if(stockCount >= 1){
+        console.log(`I'm going to eat some ${food}!`)
+    } else {
+        console.log(`We need to get some more ${food}.`)
+    }
 
 // Express the following in code: If the variable "isFavorite" is true,
 // log the statement 'My favorite food is [food]!'. Otherwise, log the
 // statement 'I recently ate [food]'. In either case, the value stored in
 // the "food" variable should be included in the logged statement.
 
-
+    if(isFavorite === true){
+        console.log(`My favorite food is ${food}!`)
+    } else {
+        console.log(`I recently ate ${food}.`)
+    }
 
 //------------------------- Arrays -------------------------//
 
