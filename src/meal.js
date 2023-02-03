b50d03abc66afdab783665b22139b030ab8cb28f
Nathan Chamberlain <chamberlainuxdesign@gmail.com>
2023-02-02 18:41:38 -0800

    Adds meals almost done

--- src/meal.js
+++ src/meal.js
@@ -1,12 +1,29 @@
-function nameMenuItem() {
-
+function nameMenuItem(item) {
+  var menuItemName = `Delicious ${item}`;
+  return menuItemName;
 }
+
+function createMenuItem(item) {
+ var menuItemName = {
+    name: `${item}`,
+    price: 10.99,
+    type: "breakfast"
+ }
+    return menuItemName;
+}
+
+function addIngredients(ingredients, array) {
+  array.push(ingredients);
+  if ("cheese" == true){
+    array.filter(addIngredients);
+  }
+} 
 
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
+  createMenuItem,
+  addIngredients,
   // formatPrice,
   // decreasePrice,
   // createRecipe
