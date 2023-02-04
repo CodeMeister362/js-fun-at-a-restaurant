157cf81c533a449366bf7c9fdf5cc6b2c51acdbd
Nathan Chamberlain <chamberlainuxdesign@gmail.com>
2023-02-03 20:34:27 -0800

    Adds completed order

--- src/order.js
+++ src/order.js
@@ -1,7 +1,40 @@
+function takeOrder(orders, deliveryOrders) {
+  if(deliveryOrders.length < 3)
+  deliveryOrders.push(orders);
+}
+
+
+
+function refundOrder(order1, deliveryOrders) {
+  for(var i = 0; i < deliveryOrders.length; i++){
+    if(deliveryOrders[i].orderNumber === order1) {
+       return deliveryOrders.splice([i], 1);
+      } 
+  }
+}
+
+ function listItems(deliveryOrders) {
+  var items = [];
+  for (var i = 0; i < deliveryOrders.length; i++) {
+    items.push(deliveryOrders[i].item); 
+  }
+  return items.join(", ");
+ }
+ 
+ function searchOrder(deliveryOrders, someOrder) {
+  var notOnList = false
+  for ( var i = 0; i < deliveryOrders.length; i++) {
+    if(deliveryOrders[i].item === someOrder) {
+      notOnList = true
+    }
+    }
+    return notOnList
+  }
+
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+   takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder
 }
