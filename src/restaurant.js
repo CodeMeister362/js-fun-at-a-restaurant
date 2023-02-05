b207c61e3b74368b9423950bcb1ce695150d9e1e
Nathan Chamberlain <chamberlainuxdesign@gmail.com>
2023-02-05 14:00:17 -0800

    Adds finished restaurant file

--- src/restaurant.js
+++ src/restaurant.js
@@ -5,7 +5,9 @@ const { restart } = require("nodemon")
       name: restaurant,
       menus: {
         breakfast: [],
+
         lunch: [],
+
         dinner: []
       }
     }
@@ -18,12 +20,35 @@ const { restart } = require("nodemon")
        restaurant.menus.lunch.push(pizzaType)
     } else if (pizzaType.type === "breakfast" && restaurant.menus.breakfast.includes(pizzaType) === false) {
        restaurant.menus.breakfast.push(pizzaType)
-    }
+    } else if (pizzaType.type === "dinner" && restaurant.menus.breakfast.includes(pizzaType) === false) {
+      restaurant.menus.dinner.push(pizzaType)
+     }
   }
+
   
+  function removeMenuItem(restaurant, name, type) {
+    var result = `No one is eating our ${name} - it has been removed from the ${type} menu!`;
+    var error = `Sorry, we don't sell ${name}, try adding a new recipe!`
+    for( i = 0; i < restaurant.menus[type].length; i++) {
+      if(restaurant.menus[type][i].name === name) {
+        restaurant.menus[type].splice([i], 1);
+        return result;
+      } 
+    } return error;
+    
+  }
+      
+          
+    
+  //   console.log(restaurant.menus.breakfast);
+  // console.log(restaurant.menus.dinner);
+  // console.log(restaurant.menus.lunch);
+ 
+  
+
 
 module.exports = {
   createRestaurant, 
   addMenuItem,
-  // removeMenuItem
+  removeMenuItem
 }
