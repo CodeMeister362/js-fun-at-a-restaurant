892f5ea4cc141061cdc97148712d7a9e2ea1ac52
Nathan Chamberlain <chamberlainuxdesign@gmail.com>
2023-02-04 16:05:34 -0800

    Adds restaurant file up to removeMenuItem

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,7 +1,29 @@
+const { restart } = require("nodemon")
 
+  function createRestaurant(restaurant) {
+    var restaurant = {
+      name: restaurant,
+      menus: {
+        breakfast: [],
+        lunch: [],
+        dinner: []
+      }
+    }
+    return restaurant
+  }
+
+
+  function addMenuItem(restaurant, pizzaType) {
+    if(pizzaType.type === "lunch" && restaurant.menus.lunch.includes(pizzaType) === false) {
+       restaurant.menus.lunch.push(pizzaType)
+    } else if (pizzaType.type === "breakfast" && restaurant.menus.breakfast.includes(pizzaType) === false) {
+       restaurant.menus.breakfast.push(pizzaType)
+    }
+  }
+  
 
 module.exports = {
-  // createRestaurant, 
-  // addMenuItem,
+  createRestaurant, 
+  addMenuItem,
   // removeMenuItem
 }
