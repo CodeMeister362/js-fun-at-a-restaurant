f604d0badf02ee2f0ecdcd4bb4627ad7e3e20998
Nathan Chamberlain <chamberlainuxdesign@gmail.com>
2023-02-05 17:34:53 -0800

    Adds completed project

--- src/meal.js
+++ src/meal.js
@@ -23,13 +23,31 @@ function formatPrice(item) {
     return `$${item}`
 }
 
+
+function decreasePrice(oldPrice) {
+  var multiplyPrice = (oldPrice * .10)
+  var subtractPrice = (oldPrice - multiplyPrice)
+  return subtractPrice
+}
+
+
+function createRecipe( title, ingredients, menuItemType) {
+  var recipe = {
+    title: title,
+    ingredients: ingredients,
+    type: menuItemType
+  }
+  return recipe
+}
+
+
 module.exports = {
   nameMenuItem,
   createMenuItem,
   addIngredients,
   formatPrice,
-  // decreasePrice,
-  // createRecipe
+  decreasePrice,
+  createRecipe
 }
 
 
